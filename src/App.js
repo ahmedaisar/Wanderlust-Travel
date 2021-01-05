a7ce7a43d4c1be631e9a2059364e4690c0d60d7b
Jaiswal <rishav.jaiswal@dxc.com>
2021-01-05 21:14:00 +0530

    React Boilerplate

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Test from "./Component/Test.js"
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      Hello World!
+    </div>
+    <Test/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Component/Test.js
@@ -0,0 +1,9 @@
+function Test() {
+    return (
+      <div>
+        Hello World From Test component!
+      </div>
+    );
+  }
+  
+export default Test;
