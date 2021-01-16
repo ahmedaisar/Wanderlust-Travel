ec4f938a4bad640ec34ece0e17d257ae593c1b84
Jaiswal <rishav.jaiswal@dxc.com>
2021-01-16 21:35:57 +0530

    Homepage Revamped

--- src/Component/Homepage/Homepage.js
+++ src/Component/Homepage/Homepage.js
@@ -1,17 +1,23 @@
 import { Link } from "react-router-dom";
 import Styles from "./Homepage.module.css";
-
+import logo from "./logo.png";
 function Homepage() {
   return (
     <div className={Styles.container}>
-      <h1 className={Styles.message}>Wanderlust!</h1>
-      <div className={Styles.btnContainer}>
-        <Link to="/login">
-          <button className={Styles.btn}>Login</button>
-        </Link>
-        <Link to="/register">
-          <button className={Styles.btn}>Register</button>
-        </Link>
+      <div className={Styles.navbar}>
+        <ul className={Styles.navWrapper}>
+          <li className={Styles.navLinks}>
+            <Link to="/login">Login</Link>
+          </li>
+          <li className={Styles.navLinks}>
+            <Link to="/register">Register</Link>
+          </li>
+        </ul>
+      </div>
+      <div className={Styles.welcomeMessage}>
+        <img src={logo} alt="logo" width="100" height="100" />
+        <h1 className={Styles.name}>Wanderlust</h1>
+        <p>Welcome to our website!</p>
       </div>
     </div>
   );
